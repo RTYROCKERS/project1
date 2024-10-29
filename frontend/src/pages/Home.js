efb6b9a400e98ab5265da19189f7b4b434cc7c60
Prakash <prakashkr9279@gmail.com>
2024-10-29 16:13:30 +0530

    first commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { handleError, handleSuccess } from '../utils';
+import { ToastContainer } from 'react-toastify';
+
+function Home() {
+    const [loggedInUser, setLoggedInUser] = useState('');
+    const [userType, setUserType] = useState('');
+    const [products, setProducts] = useState('');
+    const navigate = useNavigate();
+    useEffect(() => {
+        setLoggedInUser(localStorage.getItem('loggedInUser'));
+        setUserType(localStorage.getItem('userType'));
+    }, [])
+
+    const handleLogout = (e) => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('loggedInUser');
+        localStorage.removeItem('userType');
+        handleSuccess('User Loggedout');
+        setTimeout(() => {
+            navigate('/login');
+        }, 1000)
+    }
+
+    const fetchProducts = async () => {
+        try {
+            const url = "${process.env.REACT_APP_API_URL}/products";
+            const headers = {
+                headers: {
+                    'Authorization': localStorage.getItem('token')
+                }
+            }
+            const response = await fetch(url, headers);
+            const result = await response.json();
+            console.log(result);
+            setProducts(result);
+        } catch (err) {
+            handleError(err);
+        }
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    return (
+        <div>
+            <h1>Welcome {loggedInUser} ({userType})</h1> {/* Display userType */}
+            <button onClick={handleLogout}>Logout</button>
+            <div>
+                {userType === 'customer' ? (
+                    <h2>Schedule your scrap</h2> // Message for customers
+                ) : (
+                    <h2> Is there any customer selling scrap</h2> // Message for dealers
+                )}
+            </div>
+            <div>
+                {products && products.map((item, index) => (
+                    <ul key={index}>
+                        <li>{item.name} : {item.price}</li>
+                    </ul>
+                ))}
+            </div>
+            <ToastContainer />
+        </div>
+    )
+}
+
+export default Home
